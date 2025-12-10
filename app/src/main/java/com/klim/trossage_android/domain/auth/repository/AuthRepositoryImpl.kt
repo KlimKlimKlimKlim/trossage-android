741fbf68882ec7231bf2b9df9cdcfb7ff9274ece
KlimKlimKlimKlim <trofimov.klim.e@gmail.com>
2025-12-10 05:02:08 +0300

    added an authorisation repository

---
+++ app/src/main/java/com/klim/trossage_android/domain/auth/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package com.klim.trossage_android.domain.auth.repository
+
+import com.klim.trossage_android.domain.auth.model.AuthToken
+import com.klim.trossage_android.domain.auth.model.UserCredentials
+
+interface AuthRepositoryImpl {
+    suspend fun login(credentials: UserCredentials): AuthToken
+    suspend fun register(credentials: UserCredentials): AuthToken
+    suspend fun refreshToken(): AuthToken
+    suspend fun isLoggedIn(): Boolean
+    suspend fun getAccessToken(): String?
+}
