653a1af5836d64cd853b01d0e448f1ed3c77d1ac
KlimKlimKlimKlim <trofimov.klim.e@gmail.com>
2025-12-10 04:28:53 +0300

    added Result.kt

---
+++ app/src/main/java/com/klim/trossage_android/core/util/Result.kt
@@ -0,0 +1,9 @@
+package com.klim.trossage_android.core.util
+
+sealed class Result <out T>{
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(
+        val message: String,
+        val exeption: Throwable? = null
+    ) : Result<Nothing>()
+}
