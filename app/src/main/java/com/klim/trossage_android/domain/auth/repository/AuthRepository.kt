94ea709030baf728db483585285f76b199a64422
KlimKlimKlimKlim <trofimov.klim.e@gmail.com>
2025-12-10 05:14:02 +0300

    renamed

--- app/src/main/java/com/klim/trossage_android/domain/auth/repository/AuthRepository.kt
+++ app/src/main/java/com/klim/trossage_android/domain/auth/repository/AuthRepository.kt
@@ -3,7 +3,7 @@ package com.klim.trossage_android.domain.auth.repository
 import com.klim.trossage_android.domain.auth.model.AuthToken
 import com.klim.trossage_android.domain.auth.model.UserCredentials
 
-interface AuthRepositoryImpl {
+interface AuthRepository {
     suspend fun login(credentials: UserCredentials): AuthToken
     suspend fun register(credentials: UserCredentials): AuthToken
     suspend fun refreshToken(): AuthToken
